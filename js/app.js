68a4fcd3f16d5d615427a8992e4df91129954206
Albertina75 <145276835+Albertina75@users.noreply.github.com>
2023-10-19 13:23:02 +0200

    done random colors

--- js/app.js
+++ js/app.js
@@ -1,2 +1,31 @@
 // Implementar solución aquí
+function changeColor () {
+console.log('Me has hecho clic!');
 
+    // Obtener un color hexadecimal aleatorio
+let randomHexValue = randomHexColor();
+
+    // Cambiar el color de fondo del body
+document.querySelector('body').style.backgroundColor = randomHexValue;
+
+    // Actualizar el valor hexadecimal en el título
+document.querySelector('#hex-value').textContent = randomHexValue;
+
+}
+
+    
+
+
+
+function randomHexColor() {
+const hexValues = '#0123456789ABCDEF';
+    let result= '#';
+
+        // Generar un color hexadecimal aleatorio de 6 dígitos
+    for (let i = 0; i < 6; i++) {
+        result += hexValues[Math.floor(Math.random() * 16)];
+        
+    }
+    return result;
+
+}
